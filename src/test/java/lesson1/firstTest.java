cf2dc890cd2554e2921a3a70212c6a7414c42038
User <User@DESKTOP-ET7SJ4U>
2024-08-04 16:15:07 +0530

    First Test case

--- src/test/java/lesson1/firstTest.java
+++ src/test/java/lesson1/firstTest.java
@@ -1,5 +1,8 @@
 package lesson1;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
 public class firstTest {
 	
 	/*
@@ -13,8 +16,27 @@ public class firstTest {
 		
 		// 1. Launch browser (chrome)
 		
+		WebDriver driver = new ChromeDriver();
 		
-
+		// 2. Open URL : https://demo.opencart.com/
+		
+		driver.get("https://demo.opencart.com/");
+		
+		// 3. Validate title : "Your Store"
+		
+		String act_title = driver.getTitle();
+		if(act_title.equals("Your Store"))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+		
+		// 4. Close browser
+		
+		driver.close();
 	}
 
 }
