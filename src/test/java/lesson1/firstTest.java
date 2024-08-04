9afce984613eee79cebebc28c1a3954c5700f1a5
User <User@DESKTOP-ET7SJ4U>
2024-08-04 16:02:37 +0530

    second commit

--- src/test/java/lesson1/firstTest.java
+++ src/test/java/lesson1/firstTest.java
@@ -1,9 +1,19 @@
 package lesson1;
 
 public class firstTest {
+	
+	/*
+	1. Launch browser (chrome)
+	2. Open URL : https://demo.opencart.com/
+	3. Validate title : "Your Store"
+	4. Close browser
+	*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 1. Launch browser (chrome)
+		
+		
 
 	}
 
